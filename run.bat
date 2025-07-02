ccec5d0258ea9a96c5f2ee723419348f753a9122
cndaqianng <who@cndaqiang.ac.cn>
2025-07-02 16:02:53 +0800

    Automated commit by bot: Wed Jul  2 16:02:53     2025

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 %USERPROFILE%\miniconda3\python.exe -u www.ablesci.com.py >> log.txt 2>&1
-%USERPROFILE%\miniconda3\python.exe -u www.xxxpt.com >> log.txt 2>&1
+%USERPROFILE%\miniconda3\python.exe -u www.xxxpt.com.py >> log.txt 2>&1
